ad603e43204ce4d202d290ae063ee459d0a43e3b
Sachin-God <113123578+Sachin-God@users.noreply.github.com>
2024-09-06 22:31:50 +0530

    ML

--- Part 1 - Data Preprocessing/ML1.py
+++ Part 1 - Data Preprocessing/ML1.py
@@ -8,10 +8,21 @@ from sklearn.impute import SimpleImputer # it is used to handle missing data by 
 dataset = pd.read_csv('Part 1 - Data Preprocessing/Data.csv') # Don't add file name directly add relative file path
 X = dataset.iloc[:, :-1].values  # feature Matrix  (iloc -> locate indexes)  [: -> select every col, :-1 -> is selecting every col except last col in selected all columns]
 Y = dataset.iloc[:, -1].values  # Dependent variable Vector - which we wanna find out
-print(X)
 
 # Handling Missing data in Data.csv Feature Matrix
 imputer = SimpleImputer(missing_values=np.nan, strategy='mean') # telling imputer that we wanna replace missing value that are nam with strategy that is mean
 imputer.fit(X[:,1:3]) #This method is called to compute the necessary statistics (like mean, median, or most frequent value) from the selected columns, which will be used later to fill in missing values
 X[:,1:3] = imputer.transform(X[:,1:3])
+
+# Encoding the Independent Variable
+from sklearn.compose import ColumnTransformer  # ColumnTransformer allows applying different preprocessing steps to different columns.
+from sklearn.preprocessing import OneHotEncoder  # OneHotEncoder transforms categorical features into binary columns (one-hot encoding).
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
 print(X)
+
+# Encoding the dependent Variable
+from sklearn.preprocessing import LabelEncoder  # transforms yes and no into 1 & 0
+le = LabelEncoder()
+Y = le.fit_transform(Y)
+print(Y)
